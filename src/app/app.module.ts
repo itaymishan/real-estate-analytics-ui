16ffc9ea4fcd884fe646f82b7a010d0225776e6b
Isaac Rousso <irousso@zango.io>
2016-09-17 19:36:58 -0400

    initial import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './home/home.component';
+import {AlertModule, DatepickerModule} from 'ng2-bootstrap/ng2-bootstrap';
+import {AgmCoreModule} from 'angular2-google-maps/core';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HomeComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        AlertModule,
+        DatepickerModule,
+        AgmCoreModule.forRoot({
+            apiKey: 'AIzaSyDmz6BTx26-5rp6sjEHRyczFjZrpaFQLBc'
+        })
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,20 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+    date:Date = new Date();
+    lat:number = 51.678418;
+    lng:number = 7.809007;
+
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+
+}
